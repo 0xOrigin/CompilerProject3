d985ca838b73adea2d6aed6eb04903843e2eb498
0xOrigin <egyahmed.ezzat120@gmail.com>
2022-05-16 11:10:59 +0200

    Prepare to remove built-in functions.

--- Controllers/ScannerUtility/Utility.cs
+++ Controllers/ScannerUtility/Utility.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net;
+using System.Text;
 using Microsoft.AspNetCore.Http;
 
 namespace CompilerProject3.Controllers.ScannerUtility
@@ -31,6 +32,19 @@ namespace CompilerProject3.Controllers.ScannerUtility
                 result += code[i];
             }
             
+            return result;
+        }
+
+        
+        public static string RemoveLastChar(string code)
+        {
+            string result = "";
+
+            for (int i = 0; i < GetLength(code) - 1; i++)
+            {
+                result += code[i];
+            }
+
             return result;
         }
 
